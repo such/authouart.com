411c6aceb916b4e8a016cbfa18a83da75a1c7873
Adrien Montfort <adrien@sorare.com>
2019-09-02 21:38:45 +0200

    Use react-draggable

--- src/components/LithoPicture/index.js
+++ src/components/LithoPicture/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-danger */
 
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
+import Draggable from 'react-draggable'; // The default
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -12,70 +13,23 @@ let zIndex = 2;
 
 const LithoPicture = ({ classes, src, title, width }) => {
   const element = useRef(null);
-  let shiftX = 0;
-  let shiftY = 0;
-  let offsetX = 0;
-  let offsetY = 0;
-  let mouseX = 0;
-  let mouseY = 0;
-
-  useEffect(() => {
-    const el = document.addEventListener('dragover', event => {
-      mouseX = event.clientX;
-      mouseY = event.clientY;
-    });
-
-    return document.removeEventListener('dragover', el);
-  });
-
-  const moveAt = (clientX, clientY) => {
-    element.current.style.left = `${clientX - shiftX - offsetX}px`;
-    element.current.style.top = `${clientY - shiftY - offsetY}px`;
-  };
-
-  const onDrag = () => {
-    if (mouseX > 0 && mouseY > 0 && shiftX > 0 && shiftY > 0) {
-      moveAt(mouseX, mouseY);
-    }
-  };
 
   const bringToFront = () => {
     zIndex += 1;
     element.current.style.zIndex = zIndex;
   };
 
-  const onDragStart = event => {
-    const img = new Image();
-    event.dataTransfer.setDragImage(img, 0, 0);
-
-    offsetX = element.current.parentNode.getBoundingClientRect().left;
-    offsetY = element.current.parentNode.getBoundingClientRect().top;
-
-    shiftX = event.clientX - element.current.getBoundingClientRect().left;
-    shiftY = event.clientY - element.current.getBoundingClientRect().top;
-
-    bringToFront();
-  };
-
   return (
-    <div
-      draggable
-      className={classes.root}
-      ref={element}
-      onDrag={onDrag}
-      onDragStart={onDragStart}
-      onClick={bringToFront}
-      onKeyDown={bringToFront}
-      role="button"
-      tabIndex={-1}
-    >
-      <img src={cursor} alt="cursor" className={classes.cursor} />
-      <img src={src} alt={title} className={classes.picture} width={width} />
-      <div
-        className={classes.title}
-        dangerouslySetInnerHTML={{ __html: title }}
-      />
-    </div>
+    <Draggable onMouseDown={bringToFront}>
+      <div className={classes.root} ref={element}>
+        <img src={cursor} alt="cursor" className={classes.cursor} />
+        <img src={src} alt={title} className={classes.picture} width={width} />
+        <div
+          className={classes.title}
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+      </div>
+    </Draggable>
   );
 };
 
